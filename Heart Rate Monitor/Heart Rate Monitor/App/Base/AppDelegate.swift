28a820078600851e0734726442aa4ab0e91a4e32
Dat <vandat0599@gmail.com>
2021-03-13 10:50:31 +0700

    test conflict

--- Heart Rate Monitor/Heart Rate Monitor/App/Base/AppDelegate.swift
+++ Heart Rate Monitor/Heart Rate Monitor/App/Base/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     let rootViewController: UIViewController = {
+        return SignInVC3()
         return SignInVC()
     }()
 
