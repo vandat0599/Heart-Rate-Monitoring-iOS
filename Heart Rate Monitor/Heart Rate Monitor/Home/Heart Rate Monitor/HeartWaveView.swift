47ef802060f82fcc9efcfbe96242632883c94d5d
Dat <vandat0599@gmail.com>
2021-03-15 11:35:49 +0700

    fix heart wave animation

--- Heart Rate Monitor/Heart Rate Monitor/Home/Heart Rate Monitor/HeartWaveView.swift
+++ Heart Rate Monitor/Heart Rate Monitor/Home/Heart Rate Monitor/HeartWaveView.swift
@@ -21,10 +21,19 @@ class HeartWaveView: UIView {
     }
     
     let ECGShape = CAShapeLayer()
+    var removeShape = CAShapeLayer()
     
     private func setupView() {
+        ECGShape.strokeColor = UIColor.red.cgColor
         ECGShape.fillColor = UIColor.clear.cgColor
-        layer.addSublayer(ECGShape)
+        ECGShape.lineWidth = 2
+        ECGShape.lineJoin = .round
+        ECGShape.lineCap = .round
+        removeShape.strokeColor = UIColor.red.cgColor
+        removeShape.fillColor = UIColor.clear.cgColor
+        removeShape.lineWidth = 2
+        removeShape.lineJoin = .round
+        removeShape.lineCap = .round
     }
     
     func addSingleECG(path: UIBezierPath, checkPoint: inout CGPoint) {
@@ -50,36 +59,57 @@ class HeartWaveView: UIView {
         path.addLine(to: checkPoint)
     }
     
-    func ECGDraw() {
+    func ECGDraw(color: UIColor? = .red) {
+        ECGShape.removeFromSuperlayer()
+        removeShape.removeFromSuperlayer()
+        ECGShape.removeAllAnimations()
+        removeShape.removeAllAnimations()
         let x = CGFloat(0)
         let y = center.y
         let startingPoint = CGPoint(x: x, y: y)
         let endPoint = CGPoint(x: x + bounds.width, y: y)
+        
         let path = UIBezierPath()
         path.move(to: startingPoint)
-        
-        // Logic of Drawing
-        var checkPoint = startingPoint + CGPoint(x: 20, y: 0)
-        
+        var checkPoint = startingPoint+CGPoint(x: 0, y: 0)
         addSingleECG(path: path, checkPoint: &checkPoint, scale: 1)
         addSingleECG(path: path, checkPoint: &checkPoint, scale: 1)
         addSingleECG(path: path, checkPoint: &checkPoint, scale: 1)
         addSingleECG(path: path, checkPoint: &checkPoint, scale: 1)
         path.addLine(to: endPoint)
-        
-        ECGShape.strokeColor = UIColor.red.cgColor
         ECGShape.path = path.cgPath
+        removeShape.path = path.cgPath
+        layer.addSublayer(ECGShape)
+        let startAnimation = CABasicAnimation(keyPath: "strokeStart")
+        startAnimation.fromValue = 0
+        startAnimation.toValue = 1
+        startAnimation.duration = 1
+        startAnimation.fillMode = .backwards
+        ECGShape.add(startAnimation, forKey: "Draw")
         
-        let animation = CABasicAnimation(keyPath: "strokeEnd")
-        animation.fromValue = 0
-        animation.toValue = 1
-        animation.duration = 1
-        ECGShape.add(animation, forKey: "line")
+        
+        let pathRemove = UIBezierPath()
+        pathRemove.move(to: startingPoint)
+        var checkPoint2 = startingPoint+CGPoint(x: 0, y: 0)
+        addSingleECG(path: pathRemove, checkPoint: &checkPoint2, scale: 1)
+        addSingleECG(path: pathRemove, checkPoint: &checkPoint2, scale: 1)
+        addSingleECG(path: pathRemove, checkPoint: &checkPoint2, scale: 1)
+        addSingleECG(path: pathRemove, checkPoint: &checkPoint2, scale: 1)
+        pathRemove.addLine(to: endPoint)
+        removeShape.path = pathRemove.cgPath
+        layer.addSublayer(removeShape)
+        let endAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        endAnimation.fromValue = 0
+        endAnimation.toValue = 1.0
+        endAnimation.duration = 1
+        endAnimation.fillMode = .backwards
+        endAnimation.beginTime = CACurrentMediaTime() + 0.1
+        removeShape.add(endAnimation, forKey: "Remove")
     }
     
     func addSingleECG(path: UIBezierPath, checkPoint: inout CGPoint, scale: Float) {
         let smallSquareWidth = frame.width/20/5
-        path.addLine(to: checkPoint) // 0 0
+        path.addLine(to: checkPoint)
         checkPoint = checkPoint + CGPoint(x: 4*smallSquareWidth, y: 0)
         path.addLine(to: checkPoint)
         checkPoint = checkPoint + CGPoint(x: 2*smallSquareWidth, y: -2*smallSquareWidth)
@@ -102,11 +132,9 @@ class HeartWaveView: UIView {
         path.addLine(to: checkPoint)
         checkPoint = checkPoint + CGPoint(x: 2*smallSquareWidth, y: 6*smallSquareWidth)
         path.addLine(to: checkPoint)
-        checkPoint = checkPoint + CGPoint(x: 3*smallSquareWidth, y: -smallSquareWidth)
+        checkPoint = checkPoint + CGPoint(x: 2*smallSquareWidth, y: -smallSquareWidth)
         path.addLine(to: checkPoint)
         checkPoint = checkPoint + CGPoint(x: 2*smallSquareWidth, y: smallSquareWidth)
         path.addLine(to: checkPoint)
     }
-    
-    
 }
