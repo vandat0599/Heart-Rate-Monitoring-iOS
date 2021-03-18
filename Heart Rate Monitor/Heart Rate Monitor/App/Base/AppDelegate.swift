177e63c628f5bb20e5effffca03f5cae969c5ca3
Nguyen <nguyennguyen121198@gmail.com>
2021-03-18 14:39:06 +0700

    Code UI for SignUpVC

--- Heart Rate Monitor/Heart Rate Monitor/App/Base/AppDelegate.swift
+++ Heart Rate Monitor/Heart Rate Monitor/App/Base/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     let rootViewController: UIViewController = {
-        return UINavigationController(rootViewController: SignInVC())
+        return UINavigationController(rootViewController: SignUpVC())
     }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
