85ffafc8732fa211e1d73c997b5dc5bbb731c5c5
Nguyen <nguyennguyen121198@gmail.com>
2021-04-08 14:14:36 +0700

    feat(HistoryVC) create history view UI side

---
+++ Heart Rate Monitor/Heart Rate Monitor/Home/HistoryTab/View/RecordTableViewCell/RecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecordTableViewCell.swift
+//  Heart Rate Monitor
+//
+//  Created by Thành Nguyên on 08/04/2021.
+//
+
+import UIKit
+
+class RecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
