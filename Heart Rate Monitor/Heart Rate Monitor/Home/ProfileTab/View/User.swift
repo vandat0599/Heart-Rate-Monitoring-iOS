e6e4a1425a0b9f7f97079353bc599e492469a209
AlexisPQA <ppaax1@gmail.com>
2021-05-22 23:43:42 +0700

    fix User model

--- Heart Rate Monitor/Heart Rate Monitor/Home/ProfileTab/View/User.swift
+++ Heart Rate Monitor/Heart Rate Monitor/Home/ProfileTab/View/User.swift
@@ -9,20 +9,20 @@ import Foundation
 
 struct User: Codable {
     var id: String?
-    var account: Account?
     var name: String?
     var email: String?
     var phoneNumber: String?
     var address: String?
-    var accessToken: String?
+    var createAt: String?
+    var updateAt: String?
     
     enum CodingKeys: String, CodingKey {
         case id
-        case account
         case name
         case email
-        case phoneNumber = "phone_number"
+        case phoneNumber
         case address
-        case accessToken = "access_token"
+        case createAt
+        case updateAt
     }
 }
