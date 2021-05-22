0227cee82d35451a777d4b1f7fba53f77932bc6f
Nguyen <nguyennguyen121198@gmail.com>
2021-05-22 18:28:54 +0700

    feat(Stats View) make the stats view, it work with localdb

--- Heart Rate Monitor/Heart Rate Monitor/Stats/StatsVC.swift
+++ Heart Rate Monitor/Heart Rate Monitor/Stats/StatsVC.swift
@@ -11,11 +11,18 @@ import Charts
 class StatsVC: BaseVC {
 
     // MARK: - Outlets
-    @IBOutlet weak var allLabelsButton: UIButton!
+    @IBOutlet weak var labelButton: UIButton!
     @IBOutlet weak var weekButton: UIButton!
     @IBOutlet weak var monthButton: UIButton!
     @IBOutlet weak var chartView: BarChartView!
     @IBOutlet weak var statsView: UIView!
+    @IBOutlet weak var labelPickerView: UIPickerView!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var avgLabel: UILabel!
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var minDateLabel: UILabel!
+    @IBOutlet weak var maxDateLabel: UILabel!
     
     // MARK: - Properties
     lazy var viewModel = StatsVM(delegate: self)
@@ -30,49 +37,116 @@ class StatsVC: BaseVC {
     // MARK: - Set up
     func setupView() {
         
+        viewModel.loadAllLabel()
+        viewModel.reloadChart(byLabel: labelButton.titleLabel?.text ?? "ALL LABELS", byMonth: monthButton.isSelected)
+        
+        labelPickerView.isHidden = true
+        labelPickerView.delegate = self
+        labelPickerView.dataSource = self
+        labelPickerView.tintColor = .white
+        
+        toolBar.isHidden = true
+        toolBar.layer.cornerRadius = 10
+        toolBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        toolBar.layer.masksToBounds = true
+        
+        weekButton.setTitleColor(UIColor.gray, for: .normal)
+        weekButton.setTitleColor(UIColor(named: "pink"), for: .selected)
+        weekButton.isSelected = true
+        
+        monthButton.setTitleColor(UIColor.gray, for: .normal)
+        monthButton.setTitleColor(UIColor(named: "pink"), for: .selected)
+        monthButton.isSelected = false
+        
+        chartView.notifyDataSetChanged()
         chartView.delegate = self
         chartView.drawBarShadowEnabled = false
         chartView.drawValueAboveBarEnabled = false
         chartView.maxVisibleCount = 60
+        chartView.doubleTapToZoomEnabled = false
+        chartView.isUserInteractionEnabled = false
         
-        let xAxis = chartView.xAxis
-        xAxis.labelPosition = .bottom
-        xAxis.labelFont = .systemFont(ofSize: 10)
-        xAxis.granularity = 1
-        xAxis.labelCount = 7
+        chartView.xAxis.enabled = true
+        chartView.xAxis.gridColor = .clear
+        chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.labelTextColor = .white
+        chartView.xAxis.axisLineColor = .white
         
-        let leftAxisFormatter = NumberFormatter()
-        leftAxisFormatter.minimumFractionDigits = 0
-        leftAxisFormatter.maximumFractionDigits = 10
-        
-        let leftAxis = chartView.leftAxis
-        leftAxis.labelFont = .systemFont(ofSize: 10)
-        leftAxis.labelCount = 8
-        leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: leftAxisFormatter)
-        leftAxis.labelPosition = .outsideChart
-        leftAxis.spaceTop = 0.15
-        leftAxis.axisMinimum = 30
-        
-        let l = chartView.legend
-        l.horizontalAlignment = .left
-        l.verticalAlignment = .bottom
-        l.orientation = .horizontal
-        l.drawInside = false
-        l.form = .circle
-        l.formSize = 9
-        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
-        l.xEntrySpace = 4
+        chartView.rightAxis.enabled = false
+        chartView.leftAxis.labelCount = 4
+        chartView.leftAxis.axisMinimum = 40
+        chartView.leftAxis.labelTextColor = .white
+        chartView.leftAxis.gridColor = .white
+        chartView.leftAxis.gridLineDashLengths = [CGFloat](repeating: 1, count: 40)
+        chartView.leftAxis.axisLineColor = .clear
+        chartView.legend.enabled = false
     }
     
     // MARK: - Action
+    @IBAction func labelButtonTapped(_ sender: Any) {
+        labelPickerView.isHidden = false
+        toolBar.isHidden = false
+    }
+    @IBAction func cancelToolbarButtonTapped(_ sender: Any) {
+        labelPickerView.isHidden = true
+        toolBar.isHidden = true
+    }
+    
+    @IBAction func weekMonthButtonsTapped(_ sender: UIButton) {
+        if sender.isSelected { return }
+        weekButton.isSelected = !weekButton.isSelected
+        monthButton.isSelected = !monthButton.isSelected
+        viewModel.reloadChart(byLabel: labelButton.titleLabel?.text ?? "ALL LABELS", byMonth: monthButton.isSelected)
+    }
 }
 
 extension StatsVC: ChartViewDelegate {
     
 }
 
-extension StatsVC: StatsVMEvents {
-    func loadedData() {
-        
+extension StatsVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return viewModel.labelsList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return viewModel.labelsList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let label = viewModel.labelsList[row]
+        labelButton.setTitle(label, for: .normal)
+        viewModel.reloadChart(byLabel: label, byMonth: monthButton.isSelected)
+    }
+    
+}
+
+extension StatsVC: StatsVMEvents {
+    
+    func updateMinMaxAvg(min: Int, max: Int, avg: Int, minDate: String, maxDate: String) {
+        minLabel.text = min <= 200 ? "\(min)" : "--"
+        maxLabel.text = max > -1 ? "\(max)" : "--"
+        avgLabel.text = "\(avg)"
+        minDateLabel.text = minDate
+        maxDateLabel.text = maxDate
+    }
+    
+    func updateChartView(data: BarChartData, xAxisData: [String]) {
+        chartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: xAxisData)
+        chartView.xAxis.granularity = 1
+        
+        chartView.data = data
+    }
+    
+    func loadedData() {
+    }
+    
+    func loadedLabels() {
+        labelPickerView.reloadAllComponents()
+    }
+    
 }
--- Heart Rate Monitor/Heart Rate Monitor/Stats/StatsVM.swift
+++ Heart Rate Monitor/Heart Rate Monitor/Stats/StatsVM.swift
@@ -6,15 +6,137 @@
 //
 
 import UIKit
+import Charts
 
 protocol StatsVMEvents: AnyObject {
     func loadedData()
+    func loadedLabels()
+    func updateMinMaxAvg(min: Int, max: Int, avg: Int, minDate: String, maxDate: String)
+    func updateChartView(data: BarChartData, xAxisData: [String])
 }
 
 class StatsVM {
     weak var delegate: StatsVMEvents?
     
+    var labelsList = [String]()
+    var historyList = [HeartRateHistory]()
+    
     init(delegate: StatsVMEvents) {
         self.delegate = delegate
+    }
+    
+    func loadAllLabel() {
+        historyList = LocalDatabaseHandler.shared.getAllHistory()
+        labelsList = Array(Set(historyList.map { $0.label ?? "" }))
+        labelsList.insert("ALL LABELS", at: 0)
+        
+//        labelsList = LocalDatabaseHandler.shared.getAllLabels()
+        delegate?.loadedLabels()
+    }
+    
+    func reloadChart(byLabel label: String, byMonth: Bool) {
+        // This'll reload by week if isMonth = false
+        print("CHART RELOAD: LABEL: \(label) _ DURATION: \(byMonth ? "MONTH" : "WEEK")")
+        
+        
+        // Get list of history by filters
+        historyList = LocalDatabaseHandler.shared.searchHistoryByLabel(label: label)
+//        filtedHistoryList = filtedHistoryList.map { $0.createDate }
+//        print(filtedHistoryList)
+//        timeLabel.text = Date().fromTimeMilli(timeMilli: model.createDate ?? "").dateAndTimetoString()
+        var startDate = Date()
+        var endDate = Date()
+        
+        if byMonth {
+            startDate = startDate.startOfMonth()
+            endDate = endDate.endOfMonth()
+        } else { // filter by week
+            while startDate.getHumanReadableDayString() != "Sunday" { startDate = startDate.previousDate() }
+            while endDate.getHumanReadableDayString() != "Saturday" { endDate = endDate.nextDate() }
+        }
+        
+        // Filter histories in time required
+        historyList = historyList.filter { (Date().fromTimeMilli(timeMilli: $0.createDate ?? "0") >= startDate) && (Date().fromTimeMilli(timeMilli: $0.createDate ?? "0") <= endDate)}
+        
+        reloadChartData(startDate: startDate, endDate: endDate)
+        reloadMinMaxAvgHeartRate()
+    }
+    
+    func reloadChartData(startDate: Date, endDate: Date) {
+        var date = startDate
+        var recordList = [Int]() // BPM
+        var amountList = [Int]() // How many histories a day
+        var dateList = [String]() // The String of day
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        formatter.timeZone = .current
+        
+        while date <= endDate {
+            recordList.append(0)
+            amountList.append(0)
+            dateList.append(formatter.string(from: date))
+            print(dateList[dateList.count - 1])
+            date = date.nextDate()
+        }
+        
+        for history in historyList {
+            var i = 0
+            while formatter.string(from: Date().fromTimeMilli(timeMilli: history.createDate ?? "")) != dateList[i] {
+                i += 1
+            }
+            recordList[i] += history.heartRateNumber ?? 0
+            amountList[i] += 1
+        }
+        
+        
+        var entriesList = [BarChartDataEntry]()
+        
+        for i in 0..<recordList.count {
+            recordList[i] = amountList[i] > 0 ? recordList[i] / amountList[i] : 0
+            entriesList.append(BarChartDataEntry(x: Double(i + 1), y: Double(recordList[i])))
+        }
+        
+        let dataSet = BarChartDataSet(entries: entriesList)
+        dataSet.colors = [UIColor(named: "pink") ??  .white]
+        
+        let data = BarChartData(dataSet: dataSet)
+        data.barWidth = recordList.count < 8 ? 0.4 : 0.3
+        data.setDrawValues(false)
+        for i in 0..<dateList.count {
+            dateList[i] = "\(dateList[i].dropLast(5))"
+        }
+        dateList.insert("", at: 0)
+
+        delegate?.updateChartView(data: data, xAxisData: dateList)
+    }
+    
+    func reloadMinMaxAvgHeartRate() {
+        if historyList.isEmpty { return }
+        
+        var min = 300
+        var max = -1
+        var sum = 0
+        var minDate = ""
+        var maxDate = ""
+        
+        for record in historyList {
+            let heartRate = record.heartRateNumber ?? 0
+            sum += heartRate
+            if (heartRate < min) {
+                min = heartRate
+                minDate = record.createDate ?? ""
+            }
+            if (heartRate > max){
+                max = heartRate
+                maxDate = record.createDate ?? ""
+            }
+        }
+        
+        delegate?.updateMinMaxAvg(  min: min,
+                                    max: max,
+                                    avg: sum / historyList.count,
+                                    minDate: Date().fromTimeMilli(timeMilli: minDate).dateAndTimetoString(),
+                                    maxDate: Date().fromTimeMilli(timeMilli: maxDate).dateAndTimetoString())
     }
 }
