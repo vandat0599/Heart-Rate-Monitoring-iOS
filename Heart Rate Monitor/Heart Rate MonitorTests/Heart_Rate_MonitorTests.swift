ce7ff48ddbd11f7a2f8fefe7c37ee8ef6cac183e
DatVan <vandat0599@gmail.com>
2022-07-24 14:00:27 +0700

    add tests scheme

--- Heart Rate Monitor/Heart Rate MonitorTests/Heart_Rate_MonitorTests.swift
+++ Heart Rate Monitor/Heart Rate MonitorTests/Heart_Rate_MonitorTests.swift
@@ -19,8 +19,7 @@ class Heart_Rate_MonitorTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(1, 1)
     }
 
     func testPerformanceExample() throws {
